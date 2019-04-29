4c98eaf4ac4f193ec8537a8c19deefe5c72b2efd
Matheus Almeida <mat.almeida@live.com>
2019-04-29 19:12:09 -0300

    :sparkles: call cronjob on server start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import server from './server';
+import { runDaily } from '~/cronjobs';
+import { fetchBulkData } from '~/cronjobs/fetchBulkData';
+
+fetchBulkData();
+runDaily(fetchBulkData);
 
 export default server;
